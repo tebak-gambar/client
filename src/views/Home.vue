b23721810df5306216574dd4e39ce41157701e84
arnold_therigan <arnoldtherigan15@gmail.com>
2019-10-17 17:14:03 +0700

    initial setup fo boot strap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <GameRoom />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import GameRoom from '@/view4_5/GameRoom.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,GameRoom
   }
 }
 </script>
