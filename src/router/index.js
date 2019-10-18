15f87b234675ca3b644c056dd23d9248ec35821e
hacktivate <gunawannucky@gmail.com>
2019-10-18 14:01:25 +0700

    add logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Landing from "../views/Landing";
+import Home from "../views/Home";
+import Waiting from "../views/Waiting";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "landing",
+    component: Landing
+  },
+  {
+    path: "/home",
+    name: "home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/waiting/:id",
+    name: "waiting",
+    component: Waiting
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <form @submit.prevent="postRoom" action="#">
+          <div class="form-group">
+            <label for="#">Room Name</label>
+            <input v-model="roomForm.name" type="text" class="form-control" />
+          </div>
+          <button class="btn btn-primary">Create Room</button>
+        </form>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div v-for="room in rooms" :key="room.id" class="col-md-4">
+        {{ room.name }}
+        <br />
+        <button @click="join(room.id)" class="btn btn-primary">Join Room</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  computed: {
+    roomForm: {
+      get() {
+        return this.$store.state.roomForm;
+      },
+      set(val) {
+        return this.$store.commit("updateRoomForm", val);
+      }
+    },
+    rooms: {
+      get() {
+        return this.$store.state.rooms;
+      }
+    },
+    questions: {
+      get() {
+        return this.$store.state.questions;
+      }
+    }
+  },
+  methods: {
+    postRoom() {
+      this.$store.dispatch("postRoom");
+    },
+    getRooms() {
+      this.$store.dispatch("getRooms");
+    },
+    getQuestions() {
+      this.$store.dispatch("getQuestions");
+    },
+    join(id) {
+      this.$store.dispatch("join", id);
+    }
+  },
+  created() {
+    this.getRooms();
+    this.getQuestions();
   }
-}
+};
 </script>
+
+<style>
+</style>
